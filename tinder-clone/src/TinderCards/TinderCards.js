a02cf079ff05cf6b7031abadafa75cb34bd1369a
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-05-11 00:02:05 +0530

    Complete Backend

--- tinder-clone/src/TinderCards/TinderCards.js
+++ tinder-clone/src/TinderCards/TinderCards.js
@@ -1,23 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "../axios";
 
 function TinderCards() {
+  const [people, setPeople] = useState([]);
 
-  const [people, setPeople] = useState([
-    {
-      name: "Enrique Iglesias",
-      url: "https://www.dontdiewondering.com/wp-content/uploads/2021/09/Enrique-iglesia-1024x768.jpeg",
-    },
-    {
-      name: "Martin Garrix",
-      url: "https://cdn.smehost.net/rcarecordscom-usrcaprod/wp-content/uploads/2016/08/MG-Press-Photo-Summer-Days-smaller-e1557874197539.jpg",
-    },
-    {
-      name: "Shawn Mendes",
-      url: "https://media1.popsugar-assets.com/files/thumbor/AK19MHoD8cv7k9tglCxOrwbbf_Y/733x204:2802x2273/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/09/04/019/n/1922398/7c2241aa5d7048792c0e23.11693945_/i/Shawn-Mendes.jpg",
-    },
-  ]);
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+
+      setPeople(req.data);
+    }
+
+    fetchData();
+  }, [])
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing:" + nameToDelete);
@@ -40,7 +37,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
