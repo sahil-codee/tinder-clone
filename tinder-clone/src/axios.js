46da6c74d8c60718cf49cedc51cb7878092f9599
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-05-11 19:51:31 +0530

    Deploy on Heroku and Firebase

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://itstinder-backend.herokuapp.com",
+    baseURL: "https://itstinder-backend.herokuapp.com/",
 })
 
 export default instance;
